0191cf1247ce1b383a5707740e682284287619a6
rberto4 <r.bertocchi4@studenti.unibg.it>
2023-04-08 12:37:01 +0200

    Modifiche varie, icone, inizio schermata aggiungi esercizio
    Da finire + logica, implementata navigazione tra schermata
    
    Co-Authored-By: Francuini99 <126274813+Francuini99@users.noreply.github.com>

--- app/src/main/java/com/ingsw/fitnessapp/activities/MainActivity.java
+++ app/src/main/java/com/ingsw/fitnessapp/activities/MainActivity.java
@@ -2,17 +2,30 @@ package com.ingsw.fitnessapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.ingsw.fitnessapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ExtendedFloatingActionButton fab ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fab = findViewById(R.id.id_mainactivity_fab);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,NuovoEseicizioActivity.class));
+            }
+        });
     }
 }
