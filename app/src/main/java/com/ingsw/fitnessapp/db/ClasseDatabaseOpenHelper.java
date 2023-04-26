463380740e7377b5a21902f81f0ab94b70aeb0e6
rberto4 <r.bertocchi4@studenti.unibg.it>
2023-04-26 11:55:04 +0200

    Modifiche minori + package db

---
+++ app/src/main/java/com/ingsw/fitnessapp/db/ClasseDatabaseOpenHelper.java
@@ -0,0 +1,13 @@
+package com.ingsw.fitnessapp.db;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ClasseDatabaseOpenHelper  {
+
+}
--- app/src/main/java/com/ingsw/fitnessapp/fragments/FragmentWorkouts.java
+++ app/src/main/java/com/ingsw/fitnessapp/fragments/FragmentWorkouts.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.ingsw.fitnessapp.R;
 import com.ingsw.fitnessapp.classi.GiorniSettimana;
